c745da5bdbad642b0c8febfad41170cc100f1bc3
GiovanneAndrade <vanne.as@hotmail.com>
2023-03-22 20:38:18 -0300

    :sparkles:	feat: component nav

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { NavContainer } from './NavStyles'
+
+export const Nav = () => {
+  return (
+    <NavContainer>Nav</NavContainer>
+  )
+}
---
+++ src/components/nav/NavStyles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+  width: 100%;
+  height: 30px;
+  position: fixed;
+  top: 0;
+  color: #fff;
+  opacity: 1;
+  background-color: rgba(0, 0, 0, 0.782);
+  border-top: solid 0.1px #504848;
+  border-bottom: solid 0.1px #504848;
+`;
