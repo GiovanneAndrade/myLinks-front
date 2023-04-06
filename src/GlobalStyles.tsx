72b7ac0e7a3dd6e538093dd86b74ccad27b5f969
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-05 21:40:36 -0300

    :sparkles:	fix:  settings

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -8,8 +8,6 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    
-  
     background-image: linear-gradient(
     to right,
     #051937,
@@ -18,12 +16,16 @@ const GlobalStyles = createGlobalStyle`
     #1f865e,
     #87a34e
   );
+   
     font-family: sans-serif;
     font-family: 'Roboto', sans-serif;
     text-decoration: none;
  
   }
-
+  a {
+    cursor: pointer;
+    color: #6d94ef;
+  }
 `;
 
 export default GlobalStyles;
