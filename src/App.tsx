d196203dd66d0583aa62ce646b0073fd97ef1418
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-05 21:37:19 -0300

    :sparkles:	feat: signin router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import { Home } from "./pages/home/Home";
+import { Signin } from "./pages/signin/Signin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
         </Routes>
       </BrowserRouter>
     </>
