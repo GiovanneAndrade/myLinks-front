a727f47af34a048366b283cc7f71f1bb8f7bf36a
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-06 23:15:50 -0300

    :sparkles:	feat:  token dynamic

--- src/services/UserServices.tsx
+++ src/services/UserServices.tsx
@@ -1,10 +1,15 @@
 import axios from "axios";
+import { AuthContext } from "../providers/auth";
+import React, { useEffect } from "react";
 
 const APIprefix = "http://localhost:4000";
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjgwNzMxMzcxLCJleHAiOjE2ODA3NDU3NzF9.S3KasMzuKjpkTkcYHK7fenm6JS3HFfUUqBy_3LWDvmQ";
+ 
+  let token = localStorage.getItem("tokenMyLink") as any;
+  token =  JSON.parse(token);
+ 
+
 const config = {
-  headers: { authorization: `${token}` },
+  headers: { authorization: `${token?.token}` },
 };
 function postSignin(formValues: any) {
   return axios.post(`${APIprefix}/signin`, formValues);
@@ -14,11 +19,14 @@ function postLinks(value: any) {
   return axios.post(`${APIprefix}/link`, { link: value }, config);
 }
 function postCategory(value: string) {
-   let listLink = localStorage.getItem("check") as any;
-   listLink = listLink ? JSON.parse(listLink): [] 
-   
-   console.log(listLink);
-  return axios.post(`${APIprefix}/category`, { name: value, linkId:listLink }, config);
+  let listLink = localStorage.getItem("check") as any;
+  listLink = listLink ? JSON.parse(listLink) : [];
+
+  return axios.post(
+    `${APIprefix}/category`,
+    { name: value, linkId: listLink },
+    config
+  );
 }
 function getLinks() {
   return axios.get(`${APIprefix}/link`, config);
@@ -26,4 +34,4 @@ function getLinks() {
 function getCategories() {
   return axios.get(`${APIprefix}/category`, config);
 }
-export { postSignin, postLinks,getLinks, postCategory,getCategories };
+export { postSignin, postLinks, getLinks, postCategory, getCategories };
