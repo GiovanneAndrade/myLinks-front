c7f1ca43d9440908f9a984ae5e7f3dd333f2dccd
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-06 23:16:44 -0300

    :sparkles:	feat:  dynamic nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import { NavContainer } from './NavStyles'
-
+import React from "react";
+import { NavContainer, NavRight, P } from "./NavStyles";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import { useNavigate } from "react-router-dom";
 export const Nav = () => {
+  const navigate = useNavigate();
+  let name = localStorage.getItem("tokenMyLink") as any;
+  name = name ? JSON.parse(name) : [];
+  function logout(){
+    localStorage.removeItem("tokenMyLink");
+    navigate("/");
+  }
   return (
-    <NavContainer>Nav</NavContainer>
-  )
-}
+    <NavContainer>
+      <p>myLinks</p>
+      <p>home</p>
+      <NavRight>
+      <Tooltip title="edit" disableInteractive>
+          <Button style={{color:'#fff',margin:'-30px'}}>
+            <AccountCircleIcon />
+          </Button>
+        </Tooltip>
+        
+        <Tooltip title="Logout" disableInteractive>
+          <Button onClick={logout}>
+            <P>{name.name}</P>
+          </Button>
+        </Tooltip>
+      </NavRight>
+    </NavContainer>
+  );
+};
