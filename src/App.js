5f45035593e43ba736455d6e24a5a4f931821eb4
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-19 20:55:11 -0300

    :sparkles: fix:  private router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React,{ useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import { Home } from "./pages/home/Home";
 import { Signin } from "./pages/signin/Signin";
-import { RequireAuth } from "./providers/Private";
+import { RequireAuth, RequireLogin } from "./providers/Private";
 import PersistentDrawerLeft from "./components/drawer/Drawer";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("tokenMyLink");
+    if (!token) {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-           
-          <Route path="/"  element={<Signin />} />
-          <Route path="/home" element={<RequireAuth><PersistentDrawerLeft /></RequireAuth>} />
-         
+          <Route
+            path="/"
+            element={
+              <RequireLogin isAuthenticated={isAuthenticated}>
+                <Signin setIsAuthenticated={setIsAuthenticated} />
+              </RequireLogin>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <RequireAuth isAuthenticated={isAuthenticated}>
+                <PersistentDrawerLeft />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
