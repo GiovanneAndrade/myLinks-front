3b2dbccfbb84332bd910d4226f820ccf206eb612
GiovanneAndrade <vanne.as@hotmail.com>
2023-04-20 15:59:27 -0300

    :sparkles: feat:  protecting route

--- src/providers/Private.js
+++ src/providers/Private.js
@@ -1,23 +1,17 @@
-import React, { useContext, useEffect } from "react";
-import { Home } from "../pages/home/Home";
-import { Signin } from "../pages/signin/Signin";
-import { AuthContext } from "./auth";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
 
-export const RequireAuth = ({ children }) => {
-  const navigate = useNavigate();
-  
-  const { user, setUser, categories } = React.useContext(AuthContext) ;
-  let listLink = localStorage.getItem("tokenMyLink");
-  listLink = JSON.parse(listLink);
-
-
- 
-  
-    if (!user && !listLink) {
-      navigate("/");
-    }
- 
-
-  return children;
+export const RequireAuth = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? (
+    children
+  ) : (
+    <Navigate to="/" replace />
+  );
+};
+export const RequireLogin = ({ isAuthenticated, children }) => {
+  return !isAuthenticated ? (
+    children
+  ) : (
+    <Navigate to="/home" replace />
+  );
 };
